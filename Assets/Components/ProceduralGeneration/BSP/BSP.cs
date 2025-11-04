8c5824c3966383f9fa30c522d50f1919ae2e0bd7
Gvbmaster <gnangndong@gaming.tech>
2025-11-04 13:30:19 +0100

    à faire

---
+++ Assets/Components/ProceduralGeneration/BSP/BSP.cs
@@ -0,0 +1,52 @@
+using Components.ProceduralGeneration;
+using Cysharp.Threading.Tasks;
+using NUnit.Framework;
+using System.Threading;
+using UnityEngine;
+using VTools.RandomService;
+
+[CreateAssetMenu(menuName = "Procedural Generation Method/BSP")]
+
+public class BSP : ProceduralGenerationMethod
+{
+    protected override async UniTask ApplyGeneration(CancellationToken cancellationToken)
+    {
+        Debug.Log("BSP");
+        var allGrid = new RectInt(xMin:0, yMin:0, Grid.Width, height: Grid.Lenght);
+        var root = new BSPNode(allGrid, RandomService);
+    }
+}
+
+
+public class BSPNode
+{
+    private RectInt _bounds;
+    private RandomService _randomService;
+    private BSPNode _child1, _child2;
+    private Vector2Int _roomSize = new (5,5);
+
+    public BSPNode(RectInt bounds, RandomService randomService)
+    {
+        _bounds = bounds;
+        _randomService = randomService;
+
+        RectInt splitBoundLeft = new RectInt(_bounds.xMin, bounds.yMin, width:_bounds.width/2, _bounds.height);
+        RectInt splitBoundRight = new RectInt(xMin:_bounds.xMin +_bounds.width/2, yMin:bounds.yMin, width: _bounds.width / 2, _bounds.height);
+
+        if (splitBoundLeft.width < _roomSize.x || splitBoundLeft.height < _roomSize.y)
+        {
+            return;
+        }
+
+        if (splitBoundRight.width < _roomSize.x || splitBoundRight.height < _roomSize.y)
+        {
+            return;
+        }
+
+
+        _child1 = new BSPNode(_bounds, _randomService);
+        _child2 = new BSPNode(_bounds, _randomService);
+
+
+    }
+}
