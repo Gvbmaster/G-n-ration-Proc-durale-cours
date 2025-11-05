cfa4e4397ec0d2f4027b52b2643e6dd8fd262452
Gvbmaster <gnangndong@gaming.tech>
2025-11-05 10:59:12 +0100

    à faire pour demain le compléter

--- Assets/Components/ProceduralGeneration/BSP/BSP.cs
+++ Assets/Components/ProceduralGeneration/BSP/BSP.cs
@@ -3,6 +3,7 @@ using Cysharp.Threading.Tasks;
 using NUnit.Framework;
 using System.Threading;
 using UnityEngine;
+using UnityEngine.Rendering.Universal;
 using VTools.RandomService;
 
 [CreateAssetMenu(menuName = "Procedural Generation Method/BSP")]
@@ -21,6 +22,7 @@ public class BSP : ProceduralGenerationMethod
 public class BSPNode
 {
     private RectInt _bounds;
+    private RectInt? _room;
     private RandomService _randomService;
     private BSPNode _child1, _child2;
     private Vector2Int _roomSize = new (5,5);
@@ -33,22 +35,44 @@ public class BSPNode
         _randomService = randomService;
 
         RectInt splitBoundLeft = new RectInt(_bounds.xMin, bounds.yMin, width:_bounds.width/2, _bounds.height);
-        RectInt splitBoundRight = new RectInt(xMin:_bounds.xMin +_bounds.width/2, yMin:bounds.yMin, width: _bounds.width / 2, _bounds.height);
+        RectInt splitBoundRight = new RectInt(xMin:_bounds.xMin +_bounds.width/2, yMin:bounds.yMin, width: _bounds.width, _bounds.height);
+
+        RectInt splitBoundDown = new RectInt(_bounds.xMin, bounds.yMin, width: _bounds.width , _bounds.height/ 2);
+        RectInt splitBoundUp = new RectInt(xMin: _bounds.xMin, yMin: bounds.yMin + bounds.yMin / 2, width: _bounds.width, _bounds.height / 2);
 
         if (splitBoundLeft.width < _roomSize.x || splitBoundLeft.height < _roomSize.y)
         {
+            CreateRooms();
             return;
         }
 
         if (splitBoundRight.width < _roomSize.x || splitBoundRight.height < _roomSize.y)
         {
+
             return;
         }
 
 
         _child1 = new BSPNode(_bounds, _randomService);
         _child2 = new BSPNode(_bounds, _randomService);
-
-
     }
+
+
+    public void CreateRooms()
+    {
+        if (_child1 == null && _child2 == null)
+        {
+            int roomWidth = _randomService.Range(_roomSize.x, _bounds.width); 
+            int roomHeight = _randomService.Range(_roomSize.y, _bounds.height);
+            int roomX = _bounds.xMin + _randomService.Range(0, _bounds.width - roomWidth); 
+            int roomY = _bounds.yMin + _randomService.Range(0, _bounds.height - roomHeight);
+            _room = new RectInt(roomX, roomY, roomWidth, roomHeight);
+        }
+        else 
+        { 
+            _child1?.CreateRooms(); 
+            _child2?.CreateRooms(); 
+        }
+    }
+
 }
---
+++ Assets/Components/ProceduralGeneration/Cellular Automata/Cellular Automata.cs
@@ -0,0 +1,132 @@
+﻿using Components.ProceduralGeneration;
+using Cysharp.Threading.Tasks;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+using VTools.Grid;
+using VTools.ScriptableObjectDatabase;
+
+[CreateAssetMenu(menuName = "Procedural Generation Method/Cellular Automata")]
+public class CellularAutomata : ProceduralGenerationMethod
+{
+    [Header("Cellular Automata Settings")]
+    [SerializeField] private int roomDensity;
+
+    private bool[,] map;
+    private List<Cell> AllCells;
+
+    protected override async UniTask ApplyGeneration(CancellationToken cancellationToken)
+    {
+        AllCells = new List<Cell>();
+
+        BuildGround();
+
+        InitializeMap();
+        DisplayMap(map);
+        await UniTask.Delay(GridGenerator.StepDelay, cancellationToken: cancellationToken);
+
+        for (int step = 0; step < _maxSteps; step++)
+        {
+            cancellationToken.ThrowIfCancellationRequested();
+
+            map = SimulateStep(map);
+            DisplayMap(map);
+
+            await UniTask.Delay(GridGenerator.StepDelay, cancellationToken: cancellationToken);
+        }
+
+    }
+
+    private void BuildGround()
+    {
+        var groundTemplate = ScriptableObjectDatabase.GetScriptableObject<GridObjectTemplate>("Grass");
+
+        for (int x = 0; x < Grid.Width; x++)
+        {
+            for (int z = 0; z < Grid.Lenght; z++)
+            {
+                if (!Grid.TryGetCellByCoordinates(x, z, out var chosenCell))
+                {
+                    Debug.LogError($"Unable to get cell on coordinates : ({x}, {z})");
+                    continue;
+                }
+
+                GridGenerator.AddGridObjectToCell(chosenCell, groundTemplate, false);
+            }
+        }
+    }
+
+    private void InitializeMap()
+    {
+        map = new bool[Grid.Width, Grid.Lenght];
+        var rand = new System.Random();
+
+        for (int x = 0; x < Grid.Width; x++)
+        {
+            for (int z = 0; z < Grid.Lenght; z++)
+            {
+                map[x, z] = rand.Next(0, 100) < roomDensity;
+            }
+        }
+    }
+
+    private bool[,] SimulateStep(bool[,] oldMap)
+    {
+        bool[,] newMap = new bool[Grid.Width, Grid.Lenght];
+
+        for (int x = 0; x < Grid.Width; x++)
+        {
+            for (int z = 0; z < Grid.Lenght; z++)
+            {
+                int waterNeighbors = CountWaterNeighbors(oldMap, x, z);
+
+                if (oldMap[x, z])
+                    newMap[x, z] = waterNeighbors >= 4;
+                else
+                    newMap[x, z] = waterNeighbors >= 5;
+            }
+        }
+
+        return newMap;
+    }
+
+    private int CountWaterNeighbors(bool[,] map, int x, int z)
+    {
+        int count = 0;
+        for (int nx = x - 1; nx <= x + 1; nx++)
+        {
+            for (int nz = z - 1; nz <= z + 1; nz++)
+            {
+                if (nx == x && nz == z) continue;
+
+                if (nx < 0 || nz < 0 || nx >= Grid.Width || nz >= Grid.Lenght)
+                {
+                    count++;
+                }
+                else if (map[nx, nz])
+                {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+
+    private void DisplayMap(bool[,] map)
+    {
+        var groundTemplate = ScriptableObjectDatabase.GetScriptableObject<GridObjectTemplate>("Grass");
+        var waterTemplate = ScriptableObjectDatabase.GetScriptableObject<GridObjectTemplate>("Water");
+
+        for (int x = 0; x < Grid.Width; x++)
+        {
+            for (int z = 0; z < Grid.Lenght; z++)
+            {
+                if (!Grid.TryGetCellByCoordinates(x, z, out var cell))
+                    continue;
+
+                var template = map[x, z] ? waterTemplate : groundTemplate;
+                GridGenerator.AddGridObjectToCell(cell, template, false);
+            }
+        }
+    }
+}
