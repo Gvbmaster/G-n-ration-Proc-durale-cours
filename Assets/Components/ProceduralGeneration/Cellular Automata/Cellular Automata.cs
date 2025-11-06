9b4dc767c0f31f80fb0cc4fae0c3639ce4d31085
Gvbmaster <gnangndong@gaming.tech>
2025-11-06 07:03:51 +0100

    Exo réussi
    ici

--- Assets/Components/ProceduralGeneration/Cellular Automata/Cellular Automata.cs
+++ Assets/Components/ProceduralGeneration/Cellular Automata/Cellular Automata.cs
@@ -1,6 +1,5 @@
 ﻿using Components.ProceduralGeneration;
 using Cysharp.Threading.Tasks;
-using System.Collections.Generic;
 using System.Threading;
 using UnityEngine;
 using VTools.Grid;
@@ -10,27 +9,59 @@ using VTools.ScriptableObjectDatabase;
 public class CellularAutomata : ProceduralGenerationMethod
 {
     [Header("Cellular Automata Settings")]
-    [SerializeField] private int roomDensity;
-
-    private bool[,] map;
-    private List<Cell> AllCells;
+    [SerializeField, Range(0, 100)] private int _waterDensity = 45; 
 
     protected override async UniTask ApplyGeneration(CancellationToken cancellationToken)
     {
-        AllCells = new List<Cell>();
-
         BuildGround();
 
-        InitializeMap();
-        DisplayMap(map);
+        System.Random random = new System.Random();
+
+        for (int x = 0; x < Grid.Width; x++)
+        {
+            for (int y = 0; y < Grid.Lenght; y++)
+            {
+                cancellationToken.ThrowIfCancellationRequested();
+
+                if (Grid.TryGetCellByCoordinates(x, y, out var cell))
+                {
+                    int chance = random.Next(0, 100);
+                    if (chance < _waterDensity)
+                    {
+                        AddTileToCell(cell, WATER_TILE_NAME, true);
+                    }
+                }
+            }
+        }
+
         await UniTask.Delay(GridGenerator.StepDelay, cancellationToken: cancellationToken);
 
-        for (int step = 0; step < _maxSteps; step++)
+        for (int i = 0; i < _maxSteps; i++)
         {
             cancellationToken.ThrowIfCancellationRequested();
 
-            map = SimulateStep(map);
-            DisplayMap(map);
+            for (int x = 0; x < Grid.Width; x++)
+            {
+                for (int y = 0; y < Grid.Lenght; y++)
+                {
+                    if (Grid.TryGetCellByCoordinates(x, y, out var cell))
+                    {
+                        int waterNeighbors = CountWaterNeighbors(x, y);
+
+                        // Exemple de règle simple : l’eau se propage ou disparaît selon ses voisines
+                        if (cell.GridObject.Template.Name == WATER_TILE_NAME)
+                        {
+                            if (waterNeighbors < 3)
+                                AddTileToCell(cell, GRASS_TILE_NAME, true);
+                        }
+                        else
+                        {
+                            if (waterNeighbors > 4)
+                                AddTileToCell(cell, WATER_TILE_NAME, true);
+                        }
+                    }
+                }
+            }
 
             await UniTask.Delay(GridGenerator.StepDelay, cancellationToken: cancellationToken);
         }
@@ -39,7 +70,7 @@ public class CellularAutomata : ProceduralGenerationMethod
 
     private void BuildGround()
     {
-        var groundTemplate = ScriptableObjectDatabase.GetScriptableObject<GridObjectTemplate>("Grass");
+        var groundTemplate = ScriptableObjectDatabase.GetScriptableObject<GridObjectTemplate>(GRASS_TILE_NAME);
 
         for (int x = 0; x < Grid.Width; x++)
         {
@@ -56,77 +87,29 @@ public class CellularAutomata : ProceduralGenerationMethod
         }
     }
 
-    private void InitializeMap()
-    {
-        map = new bool[Grid.Width, Grid.Lenght];
-        var rand = new System.Random();
-
-        for (int x = 0; x < Grid.Width; x++)
-        {
-            for (int z = 0; z < Grid.Lenght; z++)
-            {
-                map[x, z] = rand.Next(0, 100) < roomDensity;
-            }
-        }
-    }
-
-    private bool[,] SimulateStep(bool[,] oldMap)
-    {
-        bool[,] newMap = new bool[Grid.Width, Grid.Lenght];
-
-        for (int x = 0; x < Grid.Width; x++)
-        {
-            for (int z = 0; z < Grid.Lenght; z++)
-            {
-                int waterNeighbors = CountWaterNeighbors(oldMap, x, z);
-
-                if (oldMap[x, z])
-                    newMap[x, z] = waterNeighbors >= 4;
-                else
-                    newMap[x, z] = waterNeighbors >= 5;
-            }
-        }
-
-        return newMap;
-    }
-
-    private int CountWaterNeighbors(bool[,] map, int x, int z)
+    private int CountWaterNeighbors(int x, int y)
     {
         int count = 0;
+
         for (int nx = x - 1; nx <= x + 1; nx++)
         {
-            for (int nz = z - 1; nz <= z + 1; nz++)
+            for (int ny = y - 1; ny <= y + 1; ny++)
             {
-                if (nx == x && nz == z) continue;
-
-                if (nx < 0 || nz < 0 || nx >= Grid.Width || nz >= Grid.Lenght)
-                {
-                    count++;
-                }
-                else if (map[nx, nz])
-                {
-                    count++;
-                }
-            }
-        }
-        return count;
-    }
-
-    private void DisplayMap(bool[,] map)
-    {
-        var groundTemplate = ScriptableObjectDatabase.GetScriptableObject<GridObjectTemplate>("Grass");
-        var waterTemplate = ScriptableObjectDatabase.GetScriptableObject<GridObjectTemplate>("Water");
-
-        for (int x = 0; x < Grid.Width; x++)
-        {
-            for (int z = 0; z < Grid.Lenght; z++)
-            {
-                if (!Grid.TryGetCellByCoordinates(x, z, out var cell))
+                if (nx == x && ny == y)
                     continue;
 
-                var template = map[x, z] ? waterTemplate : groundTemplate;
-                GridGenerator.AddGridObjectToCell(cell, template, false);
+                if (Grid.TryGetCellByCoordinates(nx, ny, out var neighbor))
+                {
+                    if (neighbor.GridObject.Template.Name == WATER_TILE_NAME)
+                        count++;
+                }
+                else
+                {
+                    count++;
+                }
             }
         }
+
+        return count;
     }
 }
